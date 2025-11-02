d1aef4590282682758861b2546046af67d4c2cb5
Roland Meyer <rmy@rmy.ch>
2025-11-02 18:08:06 +0100

    Ticktock

--- src/main/kotlin/ch/rmy/secretsanta/output/StoreMappingHandler.kt
+++ src/main/kotlin/ch/rmy/secretsanta/output/StoreMappingHandler.kt
@@ -1,18 +1,18 @@
 package ch.rmy.secretsanta.output
 
 import ch.rmy.secretsanta.MappingHandler
+import ch.rmy.secretsanta.TimeProvider
 import ch.rmy.secretsanta.mapping.Mapping
 import ch.rmy.secretsanta.mapping.Match
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.encodeToStream
 import java.io.File
-import java.time.LocalDate
-import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class StoreMappingHandler(
     private val mappingsDirectory: File,
+    private val timeProvider: TimeProvider = TimeProvider.default,
 ) : MappingHandler {
 
     private val json = Json {
@@ -28,7 +28,7 @@ class StoreMappingHandler(
             matches = matches.associate { (gifter, giftee) ->
                 gifter.id to giftee.id
             },
-            year = LocalDate.now().year,
+            year = timeProvider.now().year,
         )
 
         outputFile.outputStream().use { outStream ->
@@ -37,7 +37,7 @@ class StoreMappingHandler(
     }
 
     private fun createFileName(): String {
-        val timestamp = DateTimeFormatter.ISO_DATE_TIME.format(LocalDateTime.now())
+        val timestamp = DateTimeFormatter.ISO_DATE.format(timeProvider.now())
         return "${timestamp.replace(":", "_")}.json"
     }
 }
