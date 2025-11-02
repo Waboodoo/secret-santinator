f99b902bc889dbd5cb33cc2a4f50df6b2d4bed92
Aline Abler <alinea@riseup.net>
2025-11-02 18:37:37 +0100

    Update VarietyScorerTest

--- src/test/kotlin/ch/rmy/secretsanta/scoring/VarietyScorerTest.kt
+++ src/test/kotlin/ch/rmy/secretsanta/scoring/VarietyScorerTest.kt
@@ -81,6 +81,49 @@ class VarietyScorerTest {
     }
 
     @Test
+    fun `Scores correctly against multiple matches`() {
+        val matches = setOf(
+            Match(PERSON1, PERSON2),
+            Match(PERSON2, PERSON3),
+            Match(PERSON3, PERSON4),
+            Match(PERSON4, PERSON5),
+            Match(PERSON5, PERSON6),
+            Match(PERSON6, PERSON7),
+            Match(PERSON7, PERSON8),
+            Match(PERSON8, PERSON1),
+        )
+        val partialmatches = setOf(
+            Match(PERSON1, PERSON2),
+            Match(PERSON2, PERSON3),
+            Match(PERSON3, PERSON4),
+            Match(PERSON4, PERSON1),
+        )
+
+        val mapping = Mapping(
+            matches = matches.associate { (gifter, giftee) ->
+                gifter.id to giftee.id
+            },
+            year = 2024
+        )
+        val mapping2 = Mapping(
+            matches = partialmatches.associate { (gifter, giftee) ->
+                gifter.id to giftee.id
+            },
+            year = 2023
+        )
+
+
+        val scorer = VarietyScorer(
+            mappings = setOf(mapping, mapping2),
+            timeProvider = TimeProvider { LocalDate.of(2025, 12, 12) }
+        )
+
+        var score = scorer.score(matches)
+        assertEquals(-12, score)
+
+    }
+
+    @Test
     fun `Scores correctly against full match without year`() {
         val matches = setOf(
             Match(PERSON1, PERSON2),
